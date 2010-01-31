ea8b0893930feff7b8feed0350350af9a327e5be
Will Madden <w.a.madden@gmail.com>
2010-01-31 19:24:08 +1100

    Added Silverlib scaffolding.

---
+++ realloc8/config.rb
@@ -0,0 +1,18 @@
+# Require any additional compass plugins here.
+project_type =          :stand_alone
+
+# Local paths
+sass_dir =              "sass"
+css_dir =               "public/stylesheets"
+images_dir =            "public/images"
+javascripts_dir =       "public/javascripts"
+
+# Remote paths
+http_path =             "/"
+http_images_path =      "/images"
+http_stylesheets_path = "/stylesheets"
+http_javascripts_path = "/images"
+
+# Other options
+output_style =          :compact
+# relative_assets = true
